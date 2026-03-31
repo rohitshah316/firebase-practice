cce2df125f125acac88f8ccc95e70df20551469c
Rohit Shah <shahrohit2223@gmail.com>
2026-03-31 16:52:30 +0545

    01 Firebase Setup

---
+++ 01-firebase-setup/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {app} from './firebase'
+const App = () => {
+
+  console.log("firebase initialized:",app);
+  return (
+
+    
+    <div>
+      <h1>Firebase Setup</h1>
+     
+    </div>
+  )
+}
+
+export default App
---
+++ 01-firebase-setup/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBp2KiIBRtOWe6l3mSm4ZpuomzQMkrDmY8",
+  authDomain: "fir-practice-59cfa.firebaseapp.com",
+  projectId: "fir-practice-59cfa",
+  storageBucket: "fir-practice-59cfa.firebasestorage.app",
+  messagingSenderId: "797658635620",
+  appId: "1:797658635620:web:617caf5ba1b65c424eee71"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
